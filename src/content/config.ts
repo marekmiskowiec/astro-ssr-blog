cc8990ebf2f94c228a8e949c9c34532b2d644c02
Marek Tomasz Miśkowiec <miskowiec.ma@gmail.com>
2024-09-18 12:52:33 +0200

    fix article showing on main page

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,13 @@
 import { z, defineCollection } from "astro:content";
 const blogCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-        title: z.string(),,
-        pubDate: z.date(),
-        author: z.string(),
-        image: z.string(),
-        tags: z.array(z.string()),
-    }),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    author: z.string(),
+    image: z.string(),
+    tags: z.array(z.string()),
+  }),
 });
 
 export const collections = {
